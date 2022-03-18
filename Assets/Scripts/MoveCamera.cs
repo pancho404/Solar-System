54bfdd23785df56e0b168d22b2efcfb5e4fa4b1a
pancho404 <franciscosanchez2101@gmail.com>
2022-03-17 21:59:29 -0300

    Added text.

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -4,22 +4,47 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    public Vector3[] positions = new Vector3[11];
-    public GameObject[] stars = new GameObject[11];
+    public Vector3[] positions = new Vector3[13];
+    public GameObject[] stars = new GameObject[12];
     public int offset;
-
+    int actualPosition = 0;
+    public Vector3 initialPos = new Vector3 { x = 0, y = 0, z = 0 };
     private void Start()
     {
-        stars = GetComponents<GameObject>();
-        foreach (GameObject gameObject in stars)
-        { 
-        
+        stars = GameObject.FindGameObjectsWithTag("Stars");
+        for (int i = 0; i <= stars.Length; i++)
+        {
+            positions[i].x = stars[i].transform.position.x - offset;
         }
-
+        Camera.main.transform.position = initialPos;
     }
     // Update is called once per frame
     void Update()
     {
+        do
+        {
+            Camera.main.transform.position = positions[actualPosition];
 
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Camera.main.transform.position = positions[actualPosition];
+                actualPosition++;
+                if (actualPosition > 11)
+                {
+                    actualPosition = 11;
+                }
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Camera.main.transform.position = positions[actualPosition];
+                actualPosition--;
+                if (actualPosition > 0)
+                {
+                    actualPosition = 0;
+                }
+            }
+
+        } while (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.RightArrow));
     }
 }
